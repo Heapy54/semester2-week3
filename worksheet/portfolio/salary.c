45994c930253e89520d350ac5810b1e77756782e
Daniel Heap <tldl0545@leeds.ac.uk>
2026-02-10 16:42:29 +0000

    Worksheet

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -7,18 +7,45 @@
  * ID:201968718
  */
 
- int main( void ) {
+int main( void ) {
 
     // define and initialise variables for the problem data 
-    
+   int salary;
+   int NI_rate;
+   int Tax_rate;
+
+   float NI;
+   float Tax;
+   float take_home;
+   float taxable_salary;
+
+   //input the values
+   scanf("%d", &salary);
+   scanf("%d", &NI_rate);
+   scanf("%d", &Tax_rate);
+
+   //calculate NI amount
+   NI = salary*(NI_rate / 100.0);
+
+   //salary remaing after NI
+   take_home = salary - NI;
+
+   //calculate the tax
+   if (take_home > 12500){
+      taxable_salary = take_home-12500;
+      Tax = taxable_salary * (Tax_rate/100.0);
+   } else {
+      Tax = 0;
+   }
+
 
     // calculate the deductions and final take-home salary
-
+   take_home = take_home - Tax;
     // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
+   printf("Salary £%d", salary);
+   printf("NI contribution £%f",NI);
+   printf("Tax contribution £%f",Tax);
+   printf("Take home salary £%f",take_home);
 
-    return 0;
- }
+   return 0;
+}
